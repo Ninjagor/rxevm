271ac4382a8b3eeb05b00aab32498ec4a7bd8cb6
Rohit Karthik <rohitismaster@gmail.com>
2025-08-04 19:04:15 -0400

    added allocator stdlib methods

--- src/main.c
+++ src/main.c
@@ -20,5 +20,7 @@ int main(int argc, char** argv)
 {
     // Result r = assemble("/Users/rohit/rohit-project-work/rxevm/samples/test1.rasm", "/Users/rohit/rohit-project-work/rxevm/rasm-build/", false, true, true);
     Result r = assemble("/Users/rohit/rohit-project-work/rxevm/src/RiSK/stdlib/io.rasm", "/Users/rohit/rohit-project-work/rxevm/rasm-build/", false, true, true);
+    printf("\n\n\n");
+    Result r2 = assemble("/Users/rohit/rohit-project-work/rxevm/src/RiSK/stdlib/alloc.rasm", "/Users/rohit/rohit-project-work/rxevm/rasm-build/", false, true, true);
     return 0;
 }
